9afcb90b4f091e0502bc3ade7234b002dd74be4c
Tanishka Chouhan <chouhantanishka25@gmail.com>
2025-12-06 17:51:29 +0530

    fix csrf and enable media files

--- djangohq/settings.py
+++ djangohq/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
